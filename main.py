b361799aa3506fc92e9ae76bc09077d9108726af
Mh eftekhari <mh.eftekhari@gmail.com>
2023-04-13 13:43:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,39 @@ import subprocess
 import psutil
 
 
-context = {}
 
 
 def os_information():
+    context = {}
     context['host_name'] = subprocess.check_output("hostname -I", shell=True).strip().decode()
     context['linux'] = subprocess.check_output("uname -v", shell=True).strip().decode().split('~')[1]
     context['uptime'] = subprocess.check_output("uptime", shell=True).strip().decode()
     context['kernel'] = subprocess.check_output("uname -r", shell=True).strip().decode()
     context['bash_version'] = subprocess.check_output("bash --version", shell=True).strip().decode()
-    return 0
+    return context
 
 
 def cpu():
+    context = {}
     context['cpu_avg'] = os.getloadavg()
     context['cpu_information'] = subprocess.check_output("lscpu", shell=True).strip().decode()
-    return 0
+    return context
 
 
 def memory_information():
+    context = {}
     context['memory'] = subprocess.check_output("vmstat -s", shell=True).strip().decode()
     context['ram_size'] = psutil.virtual_memory().total/1024/1024/1024
     context['ram_status'] = psutil.virtual_memory()
-    return 0
+    return context
 
 
 def main():
-    os_information()
-    cpu()
-    memory_information()
-    return context
+    context = {}
+    context.update(os_information())
+    context.update(cpu())
+    context.update(memory_information())
+    print(context)
 
 
 main()
